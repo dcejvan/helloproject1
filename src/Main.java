df103267f7a6bfeb4aa39178bd30edc71b5adee4
Dennis Cejvan <dcejvan@uiowa.edu>
2024-01-18 15:56:33 -0600

    second push

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 //Without this line authograder will not run correctly
 //You can comment it while you work on the problem
 //When everything works - uncomment and submit!
-package com.gradescope.lab1pr1;
+// package com.gradescope.lab1pr1;
 public class Main {
     /*Find all the problems (bugs)
     / fix the errors (explain in comments what you did to fix it!)
@@ -19,7 +19,7 @@ public class Main {
         System.out.println(word);
         int x = 7 + 5;
         System.out.println(x);
-        int z = 1;
+        int z = 12;
         int y = z + 4;
 
         y += 1;
